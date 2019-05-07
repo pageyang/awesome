ac62c0f88116bca360dc73a09f036199eef91405
Yang <yangxianpei@51xf.cn>
2019-05-07 19:25:41 +0800

    干了这碗鸡汤

---
+++ src/main/java/com/page/awesome/controller/SoupController.java
@@ -0,0 +1,33 @@
+package com.page.awesome.controller;
+
+import com.page.awesome.dto.proto.SoupDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+/**
+ * @ClassName SoupController
+ * @Description 传递鸡汤控制器
+ * @Author yang
+ * @Date 2019/5/5 18:11
+ **/
+@RestController
+@RequestMapping(value = "/soup")
+@Slf4j
+public class SoupController {
+
+    @GetMapping("/getOneSoup")
+    public Response getOneSoup() {
+        SoupDto.Soup.Builder soupBuilder = SoupDto.Soup.newBuilder();
+        SoupDto.Soup soup = soupBuilder.setChickenSoup("能吃是福").setSoupId("1").setSoupUp(9).setSoupDown(10).build();
+        log.info("===soup:{}", soup);
+        log.info("===byte soup:{}", soup.toByteString());
+//        System.out(encodeBase64(soup.toByteArray()));
+        return Response.ok(soup.toString(), MediaType.APPLICATION_OCTET_STREAM).status(200).build();
+    }
+}
---
+++ src/main/java/com/page/awesome/dto/req/ChickenReqDto.java
@@ -0,0 +1,14 @@
+package com.page.awesome.dto.req;
+
+import lombok.Data;
+
+/**
+ * @ClassName ChickenReqDto
+ * @Description TODO
+ * @Author yang
+ * @Date 2019/5/7 19:21
+ **/
+@Data
+public class ChickenReqDto {
+    private String
+}
